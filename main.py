6a8784a53df9f5ca7cab3740d5be8e8fffb06b1b
nagima Dubanaeva <98920760+nagimad@users.noreply.github.com>
2022-05-17 03:42:07 +0000

    completed

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dogFact import dog
 from math_number import number
 
 def main():
-  instructions = 'Hello! This program generates a number, which is an answer to a random math equation. It then uses that number to generate either an anime quote or a dog fact. Have fun!'
+  instructions = 'Hello! This program generates a number, which is an answer to a random math addition equation. It then uses that number to generate either an anime quote or a dog fact. Have fun!'
   print(instructions)
 
   answer = int(input("Please enter your number from above: \n"))
--- math_number.py
+++ math_number.py
@@ -1,6 +1,6 @@
 import requests
 
-response = requests.get('https://x-math.herokuapp.com/api/random?max=99&min=1')
+response = requests.get('https://x-math.herokuapp.com/api/add?max=50&min=1')
    
 number=response.json()["answer"]
 print(f'Your number is: {number}\n')
